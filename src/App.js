ff5237b073150bc06a8c8a2fbd9abc2cffe1a91a
Jason Ganub <jasonganub@gmail.com>
2021-07-11 00:20:51 +0800

    Create function for Todolist with list items using todo prop

--- src/App.js
+++ src/App.js
@@ -2,12 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const todos = [
+    { id: 1, text: "Wash dishes", done: false },
+    { id: 2, text: "Do laundry", done: false },
+    { id: 3, text: "Take shower", done: false }
+  ];
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Todo List</h1>
+      <TodoList todos={todos} />
     </div>
   );
+}
+
+function TodoList(props) {
+  return (
+    <ul>
+      {props.todos.map(todo => (
+        <li>{todo.text}</li>
+      ))}
+    </ul>
+  )
 }
 
 export default App;
